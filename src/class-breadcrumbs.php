8389bb50108320d13859f2dca12d4ab14dd129b9
Justin Tadlock <justintadlock@gmail.com>
2018-07-06 13:26:27 -0500

    Flesh out the HTML and allow the dev/user to input custom tags, classes, etc. to customize the output of the breadcrumb trail.

--- src/class-breadcrumbs.php
+++ src/class-breadcrumbs.php
@@ -55,7 +55,7 @@ class Breadcrumbs implements BreadcrumbsContract {
 			'labels'          => [],
 			'post_taxonomy'   => [],
 			'show_on_front'   => false,
-			'show_title'      => true,
+			'show_trail_end'  => true,
 			'network'         => false,
 			'before'          => '',
 			'after'           => '',
@@ -94,21 +94,21 @@ class Breadcrumbs implements BreadcrumbsContract {
 	protected function defaultLabels() {
 
 		return [
-			'browse'              => esc_html__( 'Browse:',                               'hybrid-core' ),
-			'aria_label'          => esc_attr_x( 'Breadcrumbs', 'breadcrumbs aria label', 'hybrid-core' ),
-			'home'                => esc_html__( 'Home',                                  'hybrid-core' ),
-			'error_404'           => esc_html__( '404 Not Found',                         'hybrid-core' ),
-			'archives'            => esc_html__( 'Archives',                              'hybrid-core' ),
+			'title'               => __( 'Browse:',                               'hybrid-core' ),
+			'aria_label'          => _x( 'Breadcrumbs', 'breadcrumbs aria label', 'hybrid-core' ),
+			'home'                => __( 'Home',                                  'hybrid-core' ),
+			'error_404'           => __( '404 Not Found',                         'hybrid-core' ),
+			'archives'            => __( 'Archives',                              'hybrid-core' ),
 			// Translators: %s is the search query.
-			'search'              => esc_html__( 'Search results for: %s',                'hybrid-core' ),
+			'search'              => __( 'Search results for: %s',                'hybrid-core' ),
 			// Translators: %s is the page number.
-			'paged'               => esc_html__( 'Page %s',                               'hybrid-core' ),
+			'paged'               => __( 'Page %s',                               'hybrid-core' ),
 			// Translators: %s is the page number.
-			'paged_comments'      => esc_html__( 'Comment Page %s',                       'hybrid-core' ),
+			'paged_comments'      => __( 'Comment Page %s',                       'hybrid-core' ),
 			// Translators: Minute archive title. %s is the minute time format.
-			'archive_minute'      => esc_html__( 'Minute %s',                             'hybrid-core' ),
+			'archive_minute'      => __( 'Minute %s',                             'hybrid-core' ),
 			// Translators: Weekly archive title. %s is the week date format.
-			'archive_week'        => esc_html__( 'Week %s',                               'hybrid-core' ),
+			'archive_week'        => __( 'Week %s',                               'hybrid-core' ),
 
 			// "%s" is replaced with the translated date/time format.
 			'archive_minute_hour' => '%s',
@@ -159,40 +159,91 @@ class Breadcrumbs implements BreadcrumbsContract {
 	 */
 	public function fetch() {
 
+		$html = $list = $title = '';
+
 		$crumbs = $this->builder->all();
+
+		// HTML allowed in labels. Everything else gets stripped out.
+		$allowed_html = [
+			'abbr'    => [ 'title' => true ],
+			'acronym' => [ 'title' => true ],
+			'code'    => true,
+			'em'      => true,
+			'strong'  => true,
+			'i'       => true,
+			'b'       => true
+		];
 
 		if ( $crumbs ) {
 
-			$html = '<nav class="breadcrumbs">';
-			$html .= sprintf( '<h2 class="breadcrumbs__title">%s</h2>', __( 'Browse:', 'hybrid-core' ) );
+			$count     = count( $crumbs );
+			$i         = 1;
+			$show_last = $this->option( 'show_trail_end' );
 
-			$html .= '<ul class="breadcrumbs__trail">';
-
+			// Loop through each of the crumbs and build out a list.
 			foreach ( $crumbs as $crumb ) {
 
-				$url   = $crumb->url();
-				$label = $crumb->label();
-
-				if ( $url ) {
-					$html .= sprintf(
-						'<li class="breadcrumbs__crumb"><a href="%s">%s</a></li>',
-						esc_url( $url ),
-						$label
-					);
-				} else {
-					$html .= sprintf(
-						'<li><span>%s</span></li>',
-						$label
-					);
+				// Break out of the loop if this is the last item
+				// and we're not supposed to show the trail end.
+				if ( $i === $count && ! $show_last ) {
+					break;
 				}
+
+				// Filter out any unwanted HTML from the label.
+				$item = wp_kses( $crumb->label(), $allowed_html );
+
+				// Get the crumb URL.
+				$url = $crumb->url();
+
+				// Wrap the label with a link if the crumb has
+				// one and this isn't the last item.
+				if ( $url && $i !== $count  ) {
+					$item = sprintf( '<a href="%s">%s</a>', esc_url( $url ), $item );
+				}
+
+				// Build the list item.
+				$list .= sprintf(
+					'<%1$s class="%2$s">%3$s</%1$s>',
+					tag_escape( $this->option( 'item_tag' ) ),
+					esc_attr( $this->option( 'item_class' ) ),
+					$item
+				);
+
+				++$i;
 			}
 
-			$html .= '</ul></nav>';
+			// Build the list HTML.
+			$list = sprintf(
+				'<%1$s class="%2$s">%3$s</%1$s>',
+				tag_escape( $this->option( 'list_tag' ) ),
+				esc_attr( $this->option( 'list_class' ) ),
+				$list
+			);
 
-			return $html;
+			// Build the title HTML only if there's a label for it.
+			if ( $this->label( 'title' ) ) {
+
+				$title = sprintf(
+					'<%1$s class="%2$s">%3$s</%1$s>',
+					tag_escape( $this->option( 'title_tag' ) ),
+					esc_attr( $this->option( 'title_class' ) ),
+					$this->label( 'title' )
+				);
+			}
+
+			// Build out the final breadcrumbs trail HTML.
+			$html = sprintf(
+				'<%1$s class="%2$s">%3$s%4$s</%1$s>',
+				tag_escape( $this->option( 'container_tag' ) ),
+				esc_attr( $this->option( 'container_class' ) ),
+				$title,
+				$list
+			);
+
+			$html = $this->option( 'before' ) . $html . $this->option( 'after' );
 		}
 
-		return '';
+		return apply_filters( "hybrid/breadcrumbs/trail", $html, $crumbs, $this );
 	}
 
 	/**
