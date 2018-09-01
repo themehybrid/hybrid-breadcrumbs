03be4c783d68c8a2c5c18d2ae7abc8387c1ad3a4
Justin Tadlock <justintadlock@gmail.com>
2018-09-01 14:16:58 -0500

    Follow a PSR-4 autoloading system.

--- src/bootstrap-autoload.php
+++ src/bootstrap-autoload.php
@@ -24,43 +24,15 @@ spl_autoload_register( function( $class ) {
 		return;
 	}
 
-	$file       = '';
-	$new_pieces = [];
-
 	// Remove the namespace.
 	$class = str_replace( $namespace, '', $class );
 
-	// Explode the full class name into an array of items by sub-namespace
-	// and class name.
-	$pieces = explode( '\\', $class );
+	// Build the filename.
+	$file = __DIR__ . DIRECTORY_SEPARATOR . str_replace( '\\', DIRECTORY_SEPARATOR, $class ) . '.php';
 
-	foreach ( $pieces as $piece ) {
-
-		// Split pieces by uppercase letter.  Assume sub-namespaces and
-		// classes are in "PascalCase".
-		$pascal = preg_split( '/((?<=[a-z])(?=[A-Z])|(?=[A-Z][a-z]))/', $piece,  -1, PREG_SPLIT_NO_EMPTY );
-
-		// Lowercase and hyphenate the word pieces within a string.
-		$new_pieces[] = strtolower( join( '-', $pascal ) );
-	}
-
-	// Gets the prefix. `class-` for classes and `interface-` for interfaces.
-	$prefix = !! array_intersect( [ 'contract', 'contracts', 'interface', 'interface' ], $new_pieces )
-	          ? 'interface'
-		  : 'class';
-
-	// Pop the last item off the array and re-add it with the `class-` prefix
-	// and the `.php` file extension.  This is our class file.
-	$new_pieces[] = sprintf( '%s-%s.php', $prefix, array_pop( $new_pieces ) );
-
-	// Join all the pieces together by a forward slash. These are directories.
-	$file = join( DIRECTORY_SEPARATOR, $new_pieces );
-
-	// Append the file name to the framework directory.
-	$file = trailingslashit( __DIR__ ) . $file;
-
-	// Include the file only if it exists.
+	// If the file exists for the class name, load it.
 	if ( file_exists( $file ) ) {
 		include( $file );
 	}
+
 } );
